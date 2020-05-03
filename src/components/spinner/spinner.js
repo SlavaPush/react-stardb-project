d07b58cac6c6e8954f4aa9821dcbde96d504ee21
Vyacheslav Pyshkin <slavapyshkin1@gmail.com>
2020-05-03 23:13:28 +0300

    create components: person, planet, starship

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-2w7c7tbqytc">
+            <div className="ldio-wuganpcqqil">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
