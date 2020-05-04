5217991f008094d9b0837f0a7beb326fbbee67a2
Vyacheslav Pyshkin <slavapyshkin1@gmail.com>
2020-05-04 21:17:05 +0300

    add loading spinner

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './spinner.css';
 
 const Spinner = () => {
     return (
-        <div className="loadingio-spinner-double-ring-2w7c7tbqytc">
-            <div className="ldio-wuganpcqqil">
-                <div></div>
-                <div></div>
+            <div className="loadingio-spinner-double-ring-2w7c7tbqytc">
+                <div className="ldio-wuganpcqqil">
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
-        </div>
     );
 };
 
