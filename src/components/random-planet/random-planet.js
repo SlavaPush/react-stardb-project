d328ee9fdb74ca0d2c68a9e4b76dfbd272feff5a
Vyacheslav Pyshkin <slavapyshkin1@gmail.com>
2020-05-07 19:33:08 +0300

    add propTypes

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -14,15 +14,7 @@ export default class RandomPlanet extends Component {
     };
 
     static propTypes = {
-        updateInterval: (props, propName, componentName) => {
-            const value = props[propName];
-
-            if (typeof value === 'number' && !isNaN(value)){
-                return null;
-            }
-
-            return new TypeError(`${componentName}: ${propName} must be number`)
-        }
+        updateInterval: PropTypes.number
     };
 
     swapiService = new SwapiService();
